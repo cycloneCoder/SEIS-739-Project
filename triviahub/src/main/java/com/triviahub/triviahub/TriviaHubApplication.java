2b2a44dbc3f6f55cafab99bb5a64e3e0d22e675a
cycloneCoder <tibe3927@stthomas.edu>
2025-10-02 17:55:00 -0500

    Initial commit

---
+++ triviahub/src/main/java/com/triviahub/triviahub/TriviaHubApplication.java
@@ -0,0 +1,13 @@
+package com.triviahub.triviahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriviaHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriviaHubApplication.class, args);
+	}
+
+}
